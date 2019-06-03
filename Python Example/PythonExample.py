c0933c118a603ab279cdae45859df7589e26e8a1
Diogo Souza <di0g0-dg@Hotmail.com>
2019-06-03 14:46:14 -0300

    Arquivo utilizado nos últimos testes.

--- Python Example/PythonExample.py
+++ Python Example/PythonExample.py
@@ -14,7 +14,7 @@ def createPacket(deviceToken, sensor, timestamp, valor):
 
 	return packet
 
-deviceToken = "fqhsnpf80w6lm1rfc8v8"
+deviceToken = "r2334t24djeb1ku3o4j84"
 
 sensor1 = "Temperatura"
 timestamp1 = str(datetime.datetime.now())[:-3] + "Z"
@@ -35,21 +35,21 @@ valor3 = 66
 #packetLuminosidade = createPacket(deviceToken, sensor3, timestamp3, valor3);
 
 
-while True:
+# while True:
 
-	allTimeStamp = str(datetime.datetime.utcnow().replace(microsecond=0).isoformat()) + ".000Z"
+# 	allTimeStamp = str(datetime.datetime.utcnow().replace(microsecond=0).isoformat()) + ".000Z"
 
-	valueR1 = random.randint(20,30)
-	valueR2 = random.randint(20,80)
-	valueR3 = random.randint(20,800)
+# 	valueR1 = random.randint(20,30)
+# 	valueR2 = random.randint(20,80)
+# 	valueR3 = random.randint(20,800)
 
-	packetTemperatura = createPacket(deviceToken, sensor1, allTimeStamp, valueR1);
-	packetSom = createPacket(deviceToken, sensor2, allTimeStamp, valueR2);
-	packetLuminosidade = createPacket(deviceToken, sensor3, allTimeStamp, valueR3);
+# 	packetTemperatura = createPacket(deviceToken, sensor1, allTimeStamp, valueR1);
+# 	packetSom = createPacket(deviceToken, sensor2, allTimeStamp, valueR2);
+# 	packetLuminosidade = createPacket(deviceToken, sensor3, allTimeStamp, valueR3);
 
-	print "Enviando Post"
-	r = requests.post("http://localhost:1880/postData", json={'post': [packetTemperatura, packetSom, packetLuminosidade]})
-	time.sleep(2) # Delay for 1 minute (60 seconds).
+# 	print "Enviando Post"
+# 	r = requests.post("http://localhost:1880/postData", json={'post': [packetTemperatura, packetSom, packetLuminosidade]}) #, packetSom, packetLuminosidade
+# 	time.sleep(2) # Delay for 1 minute (60 seconds).
 
 # One data
 #r = requests.post("http://localhost:1880/postData", json={'post': [packetTemperatura]})
@@ -64,45 +64,52 @@ while True:
 
 ############## MQTT ##########################
 
-# temperatura_topic = "/dispositivo1/temperatura"
-# som_topic = "/dispositivo1/som"
-# luminosidade_topic = "/dispositivo1/luminosidade"
-# broker_url = "127.0.0.1"
+temperatura_topic = "/dispositivo1/temperatura"
+som_topic = "/dispositivo1/som"
+luminosidade_topic = "/dispositivo1/luminosidade"
+broker_url = "127.0.0.1"
 
-# def mqtt_client_connect():
-#     print("conectando ao broker: ", broker_url)
-#     client.connect(broker_url)		#Conecta ao broker de mensagens
-#     client.loop_start()				#Inicia loop
+def mqtt_client_connect():
+    print("conectando ao broker: ", broker_url)
+    client.connect(broker_url)		#Conecta ao broker de mensagens
+    client.loop_start()				#Inicia loop
 
-# client = mqtt.Client("Raspberry")	#Cria nova instancia
-# mqtt_client_connect()			 	#Chama funcao para conectar ao broker
+client = mqtt.Client("Raspberry")	#Cria nova instancia
+mqtt_client_connect()			 	#Chama funcao para conectar ao broker
 
-# n = 0
+n = 0
 
 
-# packetTemperatura = createPacket(deviceToken, sensor1, timestamp1, valor1);
-# packetSom = createPacket(deviceToken, sensor2, timestamp2, valor2);
-# packetLuminosidade = createPacket(deviceToken, sensor3, timestamp3, valor3);
 
-# data_Temp=json.dumps(packetTemperatura)  # encode object to JSON
-# data_Som =json.dumps(packetSom)  		 # encode object to JSON
-# data_Lum =json.dumps(packetLuminosidade) # encode object to JSON
+while True:
 
-# while True:
+	allTimeStamp = str(datetime.datetime.utcnow().replace(microsecond=0).isoformat()) + ".000Z"
 
-# 	# Note that sometimes you won't get a reading and
-# 	# the results will be null (because Linux can't
-# 	# guarantee the timing of calls to read the sensor).
-# 	# If this happens try again!
-# 	if data_Temp is not None and data_Som is not None and data_Lum is not None:
-# 		print("Publicando no Topico {} : {}*C").format( temperatura_topic, data_Temp )
-# 		print("Publicando no Topico {} : {}db").format( temperatura_topic, data_Som )
-# 		print("Publicando no Topico {} : {}lum").format( temperatura_topic, data_Lum )
+	valueR1 = random.randint(20,30)
+	valueR2 = random.randint(20,80)
+	valueR3 = random.randint(20,800)
 
-# 		client.publish(temperatura_topic, data_Temp)		#publica o dado no topico Temperatura
-# 		client.publish(som_topic, data_Som)					#publica o dado no topico Som
-# 		client.publish(luminosidade_topic, data_Lum)		#publica o dado no topico Luminosidade
-# 	else:
-#     		print('Failed to get reading. Try again!')
+	packetTemperatura = createPacket(deviceToken, sensor1, allTimeStamp, valueR1);
+	packetSom = createPacket(deviceToken, sensor2, allTimeStamp, valueR2);
+	packetLuminosidade = createPacket(deviceToken, sensor3, allTimeStamp, valueR3);
 
-# 	time.sleep(10)
+	data_Temp=json.dumps(packetTemperatura)  # encode object to JSON
+	data_Som =json.dumps(packetSom)  		 # encode object to JSON
+	data_Lum =json.dumps(packetLuminosidade) # encode object to JSON
+
+	# Note that sometimes you won't get a reading and
+	# the results will be null (because Linux can't
+	# guarantee the timing of calls to read the sensor).
+	# If this happens try again!
+	if data_Temp is not None and data_Som is not None and data_Lum is not None:
+		print("Publicando no Topico {} : {}*C").format( temperatura_topic, data_Temp )
+		print("Publicando no Topico {} : {}db").format( temperatura_topic, data_Som )
+		print("Publicando no Topico {} : {}lum").format( temperatura_topic, data_Lum )
+
+		client.publish(temperatura_topic, data_Temp)		#publica o dado no topico Temperatura
+		client.publish(som_topic, data_Som)					#publica o dado no topico Som
+		client.publish(luminosidade_topic, data_Lum)		#publica o dado no topico Luminosidade
+	else:
+    		print('Failed to get reading. Try again!')
+
+	time.sleep(2)
